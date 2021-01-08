3c95806b8141751590936b506fb499951a418923
JohnZonneveld <jjczon@gmail.com>
2021-01-08 11:48:12 -0600

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    handleClick = (event) => {
+        // console.log(event.clientX, event.clientY)
+        let position = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(position)
+    }
+   
+    render () {
+        return (
+            <button onClick={this.handleClick} >Button</button>
+
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,16 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+    handleClick = (event) => {
+        event.persist()
+        setTimeout( () => {this.props.onDelayedClick(event)}, this.props.timeout)
+    }
+
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>button</button>
+        )
+    }
+}
